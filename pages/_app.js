c48ab471b9910b33699b9a9d6e9965ad062213b8
Joshan Braham <joshanpreet.singh@simbaquartz.com>
2022-07-31 02:59:13 +0530

    mark attendance

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,58 @@
 import "../styles/globals.css";
 import "antd/dist/antd.css";
+import { Spin } from "antd";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { hostUrl } from "../hostUrl";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+  const callCurrentUser = () => {
+    console.log(
+      'localStorage.getItem("accessToken"),',
+      localStorage.getItem("accessToken")
+    );
+    setLoading(true);
+    const headers = {
+      accessToken: localStorage.getItem("accessToken"),
+    };
+    console.log("headers", headers);
+    axios
+      .get(
+        `${hostUrl}/user/me`,
+
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        // if (res?.data?.success) {
+        setLoading(false);
+        //   localStorage.setItem("accessToken", res?.data?.accessToken);
+        //   localStorage.setItem("refreshToken", res?.data?.refreshToken);
+        //   router.push(`/`);
+        // }
+      })
+      .catch((err) => {
+        if (err) {
+          setLoading(false);
+        }
+      });
+  };
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      callCurrentUser();
+    }
+  }, []);
+  return (
+    <Spin spinning={loading}>
+      {" "}
+      <Component {...pageProps} />
+    </Spin>
+  );
 }
 
 export default MyApp;
---
+++ pages/intime/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Intime = () => {
+  return <div>successfully sent</div>;
+};
+
+export default Intime;
